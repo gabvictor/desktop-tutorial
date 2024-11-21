bc2ffa2538dc7ed564a37d9b7b9f74ee2fc0c2b9
gabvictor <64426475+gabvictor@users.noreply.github.com>
2024-11-20 22:21:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const statusMessage = document.getElementById('statusMessage');
 // Configurações do repositório GitHub
 const GITHUB_TOKEN = "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"; // Substitua pelo seu token pessoal
 const GITHUB_OWNER = "gabvictor";   // Substitua pelo seu nome de usuário ou organização
-const GITHUB_REPO = "https://github.com/gabvictor/desktop-tutorial/edit/main/script.js"; // Substitua pelo nome do repositório
+const GITHUB_REPO = "desktop-tutorial"; // Substitua pelo nome do repositório
 const GITHUB_FILE_PATH = "dados/formulario.json"; // Caminho do arquivo onde os dados serão armazenados
 
 form.addEventListener('submit', async (event) => {
